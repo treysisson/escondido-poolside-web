96a690c45c7a0a67c208feb0eabf3a8a553385d5
treysisson <trey.sisson@gmail.com>
2025-06-14 19:02:06 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use '/' if using a custom domain, or '/repository-name/' if using github.io subdomain
+  base: './', // Important: use relative paths
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
   },
 })
